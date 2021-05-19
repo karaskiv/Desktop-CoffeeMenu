6101f70e11aed6d1f07fab52b28ecbd55e97e8f7
Karaskiv <82354360+Karaskiv@users.noreply.github.com>
2021-05-19 23:37:48 +0700

    Initial Commit, 1/3

---
+++ Menu/src/Menu.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Menu {
+	public static void main(String[] args) {
+		// User choice
+		int choice;
+		
+		// Display menu
+		System.out.println("");
+		System.out.println(" KARASKIV COFFEEHOUSE");
+		System.out.println("======================");
+		System.out.println("        Menu");
+		System.out.println("----------------------");
+		System.out.println("1. Next Americano");
+		System.out.println("2. Spanish Latte");
+		System.out.println("3. Get Espresso");
+		System.out.println("----------------------");
+		System.out.println("4. Exit");
+		System.out.println("======================");
+		System.out.print("Enter choice: ");
+		
+		// Setup scanner
+		Scanner in = new Scanner(System.in);
+		
+		// Get choice from user
+		choice = in.nextInt();
+		
+		// Menu loop
+		while(choice != 4) {
+			// Check choice value
+			if (choice == 1) {
+				System.out.println("Next Americano coffee!");
+			}
+			else if (choice == 2) {
+				System.out.println("Spanish Latte coffee!");
+			}
+			else if (choice == 3) {
+				System.out.println("Get Espresso coffee!");
+			}
+			
+			// Display menu
+			System.out.println("");
+			System.out.println(" KARASKIV COFFEEHOUSE");
+			System.out.println("======================");
+			System.out.println("        Menu");
+			System.out.println("----------------------");
+			System.out.println("1. Next Americano");
+			System.out.println("2. Spanish Latte");
+			System.out.println("3. Get Espresso");
+			System.out.println("----------------------");
+			System.out.println("4. Exit");
+			System.out.println("======================");
+			System.out.print("Enter choice: ");
+			
+			// Get choice from user
+			choice = in.nextInt();
+		}
+		System.out.println("Goodbye, call again!");
+	}
+}
