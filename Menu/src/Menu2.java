01b767938abcdcbdc102217a8f8ac5980f99643d
Karaskiv <karaskiv@gmail.com>
2021-05-21 18:49:15 +0700

    wip: 80% done, added bill, all neat formatted

--- Menu/src/Menu2.java
+++ Menu/src/Menu2.java
@@ -15,8 +15,6 @@ public class Menu2 {
 		}
 		System.out.println("----------------------");
 		// Print only the last option and item
-		// Why is it not opts[3] instead? Cause we might add more in the menu 
-		// while Exit is always be on the last of the list
 		System.out.println(opts[opts.length-1] + ". " + items[items.length-1]);
 		System.out.println("======================");
 		System.out.print("Enter choice: ");
@@ -29,13 +27,13 @@ public class Menu2 {
 		// Menu data
 		int options[] = {1,2,3,4,5,0};
 		String menuItems[] = {"Spanish Latte","Next Fredo","Next Frappe","Get Americano","Get Espresso","","Exit"};
-		double prices[] = {20, 17.5, 17.5, 15, 15, 0};
+		double price[] = {20, 17.5, 17.5, 15, 15, 0};
 
 		// Exit option
 		int EXIT = options[options.length-1];
 
 		// Display menu
-		displayMenu(options, menuItems, prices);
+		displayMenu(options, menuItems, price);
 		
 		// Setup scanner
 		Scanner in = new Scanner(System.in);
@@ -47,29 +45,29 @@ public class Menu2 {
 		while(choice != EXIT) {
 			// Check choice value
 			if (choice == options[0]) {
-				System.out.println(menuItems[0] + " coffee!");
+				System.out.println(menuItems[0] + " coffee\t" + price[0] + "k");
 			}
 			else if (choice == options[1]) {
-				System.out.println(menuItems[1] + " coffee!");
+				System.out.println(menuItems[1] + " coffee\t" + price[1] + "k");
 			}
 			else if (choice == options[2]) {
-				System.out.println(menuItems[2] + " coffee!");
+				System.out.println(menuItems[2] + " coffee\t" + price[2] + "k");
 			}
 			else if (choice == options[3]) {
-				System.out.println(menuItems[3] + " coffee!");
+				System.out.println(menuItems[3] + " coffee\t" + price[3] + "k");
 			}
 			else if (choice == options[4]) {
-				System.out.println(menuItems[4] + " coffee!");
+				System.out.println(menuItems[4] + " coffee\t" + price[4] + "k");
 			}
 			
 			// Display menu
-			displayMenu(options, menuItems, prices);
+			displayMenu(options, menuItems, price);
 			
 			// Get choice from user
 			choice = in.nextInt();
 		}
 
-		System.out.println("Goodbye, call again!");
+		System.out.println("Thank you, have a good day!");
 
 	}
 }
